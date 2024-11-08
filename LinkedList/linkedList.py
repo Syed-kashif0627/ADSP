dc788784ef582739f0a39075b1d131fc59c54c71
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2024-11-08 16:43:18 +0530

    Add files via upload

--- LinkedList/linkedList.py
+++ LinkedList/linkedList.py
@@ -11,7 +11,27 @@ class LinkedList:
             t=Node(elem);
             t.next=self.L
             self.L=t
-            
+
+    def delFirst(self):
+        if self.L is None:
+            print("The List is Empty: ")
+            return -1
+        else:
+            t=self.L.data
+            k=self.L
+            self.L=self.L.next
+            del k
+            return t
+    def insLast(self,val):
+        if self.L is None:
+            self.L=Node(val)
+        else:
+            c=self.L
+            while c.next!=None:
+                c=c.next
+            t=Node(val)
+            c.next=t
+
 
 l1=LinkedList()
 
@@ -25,3 +45,16 @@ while curr!=None:
     print(curr.data)
     curr=curr.next
         
+deletedElem=l1.delFirst()
+print(f'Deleted First ELement from the list : {deletedElem}')
+
+l1.insLast(1)
+
+print("list after insert at last and delting from first :")
+
+curr=l1.L
+while curr!=None:
+    print(curr.data)
+    curr=curr.next
+        
+
