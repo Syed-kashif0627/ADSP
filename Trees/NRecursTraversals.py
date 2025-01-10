a677e62eab30444fec4508b04cf90e7f8663d3fd
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2025-01-10 15:38:12 +0530

    Non Recursive Preorder and Inorder Traversals

---
+++ Trees/NRecursTraversals.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            current = self.root
+            parent = None
+            while current:
+                parent = current
+                if value < current.value:
+                    current = current.left
+                else:
+                    current = current.right
+            if value < parent.value:
+                parent.left = Node(value)
+            else:
+                parent.right = Node(value)
+
+    def preorder_non_recursive(self):
+        if self.root is None:
+            return
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            print(node.value, end=' ')
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+
+    def inorder_non_recursive(self):
+        stack = []
+        current = self.root
+        while stack or current:
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                current = stack.pop()
+                print(current.value, end=' ')
+                current = current.right
+
+# Example usage
+bt = BinaryTree()
+values = [10, 5, 15, 3, 7, 12, 18]
+for value in values:
+    bt.insert(value)
+
+print("Non-recursive Preorder traversal:")
+bt.preorder_non_recursive()
+print("\nNon-recursive Inorder traversal:")
+bt.inorder_non_recursive()
