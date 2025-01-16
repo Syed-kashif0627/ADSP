d85d9a22ba9a976266136fc538f251cbae3e850c
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2025-01-16 12:11:06 +0530

    Recursive Binary Search

---
+++ Sorting/RecBinSearch.py
@@ -0,0 +1,20 @@
+def recBinSearch(arr, low, high, x):
+    if high>=low:
+        mid=(high+low)//2
+        if arr[mid]==x:
+            return mid
+        elif arr[mid]>x:
+            return recBinSearch(arr, low, mid-1, x)
+        else:
+            return recBinSearch(arr, mid+1, high, x)
+    else:
+        return -1
+
+arr=[2, 3, 4, 10, 40]
+x=10
+result=recBinSearch(arr, 0, len(arr)-1, x)
+
+if result!=-1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
