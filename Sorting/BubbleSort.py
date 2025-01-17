41e5141576a19f7793e95cb94727a741161c885e
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2025-01-17 10:04:28 +0530

    Updated

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -5,6 +5,22 @@ def bubbleSort(l,n):
             if(l[j]>l[j+1]):
                 l[j],l[j+1]=l[j+1],l[j]
 
+def binSearch(arr,key):
+    l=0
+    h=len(arr)-1
+
+    while l<=h:
+        m=(l+h)//2
+
+        if arr[m]==key:
+            return m
+        elif arr[m]<key:
+            l=m+1
+        else:
+            h=m-1
+
+    return -1
+
 
 
 n=int(input("Enter the SIze of List "))
@@ -12,6 +28,7 @@ l=[]
 for i in range(n):
     elem=int(input(f'Enter The {i+1} th Element:'))
     l.append(elem)
+
 
 print("Original List:")
 print(l)
@@ -21,3 +38,13 @@ bubbleSort(l,n)
 print("List After Bubble Sort:")
 
 print(l)
+
+k=int(input("Enter Key Value to be Searched: "))
+
+index=binSearch(l,k)
+
+if index==-1:
+    print(f"Key {k} not Found in the list!!")
+else:
+    print(f"Key {k} found at index {index}!!")
+
