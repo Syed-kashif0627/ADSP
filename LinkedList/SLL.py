bac734fbaa56be9417648516da568f00e8ee8caa
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2025-01-23 14:14:20 +0530

    Updated

--- LinkedList/SLL.py
+++ LinkedList/SLL.py
@@ -1,60 +1,83 @@
-from node import Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 class LinkedList:
     def __init__(self):
-        self.L=None
+        self.L = None
 
-    def insFirst(self,elem):
+    def insFirst(self, elem):
         if self.L is None:
-            self.L=Node(elem)
+            self.L = Node(elem)
         else:
-            t=Node(elem);
-            t.next=self.L
-            self.L=t
+            t = Node(elem)
+            t.next = self.L
+            self.L = t
 
     def delFirst(self):
         if self.L is None:
-            print("The List is Empty: ")
+            print("The List is Empty")
             return -1
         else:
-            t=self.L.data
-            k=self.L
-            self.L=self.L.next
+            t = self.L.data
+            k = self.L
+            self.L = self.L.next
             del k
             return t
-    def insLast(self,val):
+
+    def insLast(self, val):
         if self.L is None:
-            self.L=Node(val)
+            self.L = Node(val)
         else:
-            c=self.L
-            while c.next!=None:
-                c=c.next
-            t=Node(val)
-            c.next=t
+            c = self.L
+            while c.next is not None:
+                c = c.next
+            t = Node(val)
+            c.next = t
 
+    def display(self):
+        curr = self.L
+        while curr:
+            print(curr.data, end=" -> ")
+            curr = curr.next
+        print("None")
 
-l1=LinkedList()
+    def delete(self, key):
+        curr = self.L
 
-l1.insFirst(50)
-l1.insFirst(39)
-l1.insFirst(43)
-l1.insFirst(62)
+        if curr and curr.data == key:
+            self.L = curr.next
+            curr = None
+            return
 
-curr=l1.L
-while curr!=None:
-    print(curr.data)
-    curr=curr.next
-        
-deletedElem=l1.delFirst()
-print(f'Deleted First ELement from the list : {deletedElem}')
+        p = None
+        while curr and curr.data != key:
+            p = curr
+            curr = curr.next
 
-l1.insLast(1)
+        if curr is None:
+            return
 
-print("list after insert at last and delting from first :")
+        p.next = curr.next
+        curr = None
 
-curr=l1.L
-while curr!=None:
-    print(curr.data)
-    curr=curr.next
-        
+# Create a linked list
+ll = LinkedList()
 
+# Insert integers to the list
+ll.insLast(1)
+ll.insLast(2)
+ll.insLast(3)
+ll.insLast(4)
+
+# Display the list
+print("Original list:")
+ll.display()
+
+# Delete an integer from the list
+ll.delete(3)
+
+# Display the list after deletion
+print("List after deleting 3:")
+ll.display()
