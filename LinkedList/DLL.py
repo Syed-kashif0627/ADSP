fb9eef4ca46c9583f4ecf30368e7087dce1f95c6
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2025-01-23 14:37:50 +0530

    Updated

--- LinkedList/DLL.py
+++ LinkedList/DLL.py
@@ -1,3 +1,4 @@
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -8,7 +9,41 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self, data):
+    def insFirst(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+        else:
+            newNode.next = self.head
+            self.head.prev = newNode
+            self.head = newNode
+
+    def delFirst(self):
+        if self.head is None:
+            print("The List is Empty")
+            return -1
+        else:
+            t = self.head.data
+            self.head = self.head.next
+            if self.head:
+                self.head.prev = None
+            return t
+
+    def delLast(self):
+        if self.head is None:
+            print("The List is Empty")
+            return -1
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            if curr.prev:
+                curr.prev.next = None
+            else:
+                self.head = None
+            return curr.data
+
+    def insLast(self, data):
         newNode = Node(data)
         if self.head is None:
             self.head = newNode
@@ -21,10 +56,11 @@ class DoublyLinkedList:
 
     def display(self):
         curr = self.head
+        print("None<=>",end='')
         while curr:
-            print(curr.data, end=" ")
+            print(curr.data, end="<=>")
             curr = curr.next
-        print()
+        print("None\n")
 
     def delete(self, key):
         curr = self.head
@@ -46,7 +82,7 @@ dll = DoublyLinkedList()
 # a) Create a doubly linked list of integers
 integers = [1, 2, 3, 4, 5]
 for num in integers:
-    dll.append(num)
+    dll.insLast(num)
 
 # b) Traverse the list and display its contents
 print("Contents of the doubly linked list:")
@@ -56,3 +92,19 @@ dll.display()
 dll.delete(3)
 print("Contents of the doubly linked list after deleting 3:")
 dll.display()
+
+# Insert an integer at the beginning of the list
+dll.insFirst(0)
+print("Contents of the doubly linked list after inserting 0 at the beginning:")
+dll.display()
+
+# Delete the first integer from the list
+dll.delFirst()
+print("Contents of the doubly linked list after deleting the first element:")
+dll.display()
+
+# Delete the last integer from the list
+dll.delLast()
+print("Contents of the doubly linked list after deleting the last element:")
+dll.display()
+
